43a539db6ea4733b02e68ec220fb9f2d491a667d
Andrew <andrewmaleski5@gmail.com>
2023-11-17 18:19:59 -0300

    Criando o core: aprendendo a criar URL amigaveis

--- core/ConfigController.php
+++ core/ConfigController.php
@@ -3,9 +3,28 @@
 namespace Core;
 
 class ConfigController {
-
+    private array $urlArray;
+    public string $url;
+    private string $urlController;
+    private string $urlMetodo;
     public function __construct() {
-        echo "Core carregado<br>"
-;    }
+        if(!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))){
+            $this->url = filter_input(INPUT_GET,"url", FILTER_DEFAULT);
+            $this->urlArray = explode("/", $this->url);
+            
+            if((isset($this->urlArray[0])) and (isset($this->urlArray[1]))){
+                $this->urlController = $this->urlArray[0];
+            $this->urlMetodo = $this->urlArray[1];
+            }else{
+                $this->urlController = "home";
+            $this->urlMetodo = "index";
+            }
+        }else{
+            $this->urlController = "home";
+            $this->urlMetodo = "index";
+        }
+
+    
+}
 
 }
